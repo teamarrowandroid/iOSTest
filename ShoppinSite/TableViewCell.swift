ca941b8aca206ad7ce7fedd989a7ee25037234df
harpreet singh <harpreetsingh@harpreets-MacBook-Pro.local>
2017-02-21 22:13:45 -0500

    first commit

---
+++ ShoppinSite/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ShoppinSite
+//
+//  Created by harpreet singh on 2017-02-21.
+//  Copyright © 2017 assignment2. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
